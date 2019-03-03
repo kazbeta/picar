294441f14eb7911ae13cd4ccd9965f6f756eb05c
kazbeta <45856707+kazbeta@users.noreply.github.com>
2019-03-02 16:16:24 -0800

    Create control0.py

---
+++ control0.py
@@ -0,0 +1,80 @@
+import time
+from SunFounder_TB6612 import TB6612
+import RPi.GPIO as GPIO
+
+def main():
+	import time
+
+	print "********************************************"
+	print "*                                          *"
+	print "*           SunFounder TB6612              *"
+	print "*                                          *"
+	print "*          Connect MA to BCM17 -> 20            *"
+	print "*          Connect MB to BCM18 -> 16           *"
+	print "*         Connect PWMA to BCM27 -> 12          *"
+	print "*         Connect PWMB to BCM22 -> 26          *"
+	print "*                                          *"
+	print "********************************************"
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup((12, 26), GPIO.OUT)
+	a = GPIO.PWM(12, 60)
+	b = GPIO.PWM(26, 60)
+	a.start(0)
+	b.start(0)
+
+	def a_speed(value):
+		a.ChangeDutyCycle(value)
+
+	def b_speed(value):
+		b.ChangeDutyCycle(value)
+
+	motorA = TB6612.Motor(20)
+	motorB = TB6612.Motor(16)
+	motorA.debug = True
+	motorB.debug = True
+	motorA.pwm = a_speed
+	motorB.pwm = b_speed
+
+	delay = 0.05
+
+	motorA.forward()
+	for i in range(0, 101):
+		motorA.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorA.speed = i
+		time.sleep(delay)
+
+	motorA.backward()
+	for i in range(0, 101):
+		motorA.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorA.speed = i
+		time.sleep(delay)
+
+	motorB.forward()
+	for i in range(0, 101):
+		motorB.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorB.speed = i
+		time.sleep(delay)
+
+	motorB.backward()
+	for i in range(0, 101):
+		motorB.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorB.speed = i
+		time.sleep(delay)
+
+def destroy():
+	motorA.stop()
+	motorB.stop()
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		destroy()
