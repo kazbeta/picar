f000cfa0c69bc606b048858a9a668412362601a8
kazbeta <45856707+kazbeta@users.noreply.github.com>
2019-03-02 16:43:15 -0800

    Update control0.py

--- control0.py
+++ control0.py
@@ -1,20 +1,17 @@
+import sys,tty,termios
 import time
 from SunFounder_TB6612 import TB6612
 import RPi.GPIO as GPIO
 
+#Note#
+#Connect MA to BCM20
+#Connect MB to BCM16
+#Connect PWMA to BCM12
+#Connect PWMB to BCM26
+
 def main():
 	import time
-
-	print "********************************************"
-	print "*                                          *"
-	print "*           SunFounder TB6612              *"
-	print "*                                          *"
-	print "*          Connect MA to BCM17 -> 20            *"
-	print "*          Connect MB to BCM18 -> 16           *"
-	print "*         Connect PWMA to BCM27 -> 12          *"
-	print "*         Connect PWMB to BCM22 -> 26          *"
-	print "*                                          *"
-	print "********************************************"
+	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup((12, 26), GPIO.OUT)
 	a = GPIO.PWM(12, 60)
@@ -30,51 +27,95 @@ def main():
 
 	motorA = TB6612.Motor(20)
 	motorB = TB6612.Motor(16)
-	motorA.debug = True
-	motorB.debug = True
+# 	motorA.debug = True
+# 	motorB.debug = True
 	motorA.pwm = a_speed
 	motorB.pwm = b_speed
 
 	delay = 0.05
+	s = get()
+	
+	while(1):
+	if s == 0
+	#stop if nothing pressed#
+	    motorA.stop()
+	    motorB.stop()   
+	elif state == 1
+	#going forward#
+	    motorA.forward()
+	    motorA.speed = 100
+	    motorB.forward()
+	    motorB.speed = 100
+	
+	elif s == -1
+	#going backward#
+	    motorA.backward()
+	    motorA.speed = 100
+	    motorB.backward()
+	    motorB.speed = 100
+	
+	elif s == 2
+	#rotate right#	
+	    motorA.forward()
+	    motorA.speed = 100
+	    motorB.backward()
+	    motorB.speed = 100
+	
+	elif s == 3
+	#rotate left#
+	    motorA.backward()
+	    motorA.speed = 100
+	    motorB.forward()
+	    motorB.speed = 100
 
-	motorA.forward()
-	for i in range(0, 101):
-		motorA.speed = i
-		time.sleep(delay)
-	for i in range(100, -1, -1):
-		motorA.speed = i
-		time.sleep(delay)
-
-	motorA.backward()
-	for i in range(0, 101):
-		motorA.speed = i
-		time.sleep(delay)
-	for i in range(100, -1, -1):
-		motorA.speed = i
-		time.sleep(delay)
-
-	motorB.forward()
-	for i in range(0, 101):
-		motorB.speed = i
-		time.sleep(delay)
-	for i in range(100, -1, -1):
-		motorB.speed = i
-		time.sleep(delay)
-
-	motorB.backward()
-	for i in range(0, 101):
-		motorB.speed = i
-		time.sleep(delay)
-	for i in range(100, -1, -1):
-		motorB.speed = i
-		time.sleep(delay)
+	else:
+            destroy()
 
 def destroy():
 	motorA.stop()
 	motorB.stop()
 
+class _Getch:
+    def __call__(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(3)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+def get():
+    inkey = _Getch()
+    state = 0
+    while(1):
+	k=inkey()
+    if k=='\x1b[A':
+	state == 1
+        print("forward")
+    elif k=='\x1b[B':
+	state == -1
+        print("backward")
+    elif k=='\x1b[C':
+	state == 2
+	print("right")
+    elif k=='\x1b[D':
+	state == 3
+	print("left")
+    else:
+        print("not an arrow key!")
+    return state
+	
 if __name__ == '__main__':
 	try:
 		main()
 	except KeyboardInterrupt:
 		destroy()
+
+# def main():
+#     for i in range(0,20):
+#         get()
+
+# if __name__=='__main__':
+#     main()
